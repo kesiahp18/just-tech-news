d5bad31c5d28b0d0451e52f1d083fd3538b9f40d
kesiahp18 <kesiahp18@gmail.com>
2021-01-27 16:56:41 -0500

    hash passwords on post or password update

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {}
 
@@ -37,6 +38,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         //TABLE CONFIGURATION OPTIONS HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
         sequelize,
         //pass in imported sequelize connection (the direct connection to database)
