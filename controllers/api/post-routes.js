8e47954049c87eb7fe450d4dd342916fd245f771
kesiahp18 <kesiahp18@gmail.com>
2021-02-06 17:57:36 -0500

    fixed upvote

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -85,12 +85,15 @@ router.post('/', (req, res) => {
 
 //vote on a post
 router.put('/upvote', (req, res) => {
-    Post.upvote(req.body, {Vote})
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-    });
+    // custom static method created in models/Post.js
+    if (req.session) {
+      Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+        .then(updatedVoteData => res.json(updatedVoteData))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    }
 });
 
 
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session(sess));
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
